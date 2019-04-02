6263dc680b560829afd2095648d3dcb83353584e
Zane2601 <d.zanelli002@studenti.unibs.it>
2019-04-02 15:46:44 +0200

    boh

--- Planetarium/src/it/unibs/fp/planetarium/PlanetariumMain.java
+++ Planetarium/src/it/unibs/fp/planetarium/PlanetariumMain.java
@@ -3,7 +3,101 @@ package it.unibs.fp.planetarium;
 import it.unibs.fp.mylib.InputDati;
 import it.unibs.fp.mylib.MyMenu;
 
+
 public class PlanetariumMain {
+
+    private static final String RICHIESTA_POSIZIONE_CORPO_CELESTE = "Inserisci la poszione del corpo celeste rispetto alla stella (0, 0): ";
+    private static final String RICHIESTA_MASSA_CORPO_CELESTE = "Inserisci la massa del corpo celeste: ";
+    private static final String RICHIESTA_NOME_CORPO_CELESTE = "Inserisci il nome del corpo celeste: ";
+    private static final String BENVENUTO_CREA_LUNA = "Stai per creare una nuova luna";
+    private static final String BENVENUTO_CREA_PIANETA = "Stai per creare un nuovo pianeta";
+    
+    private static final String TITOLO_MENU = "Scegli un'azione";
+    private static final String [] VOCI_MENU = {
+            "Crea un pianeta",
+            "Crea una luna",
+            "Elimina un pianeta",
+            "Elimina una luna"
+    };
+    private static final String SALUTO_INIZIALE = "Benvenuto in Planetarium!";
+
+    
+    //*************************************************************************************
+    public static void main(String[] args) {
+        int scelta = 0;
+        int i = 0;
+        int j = 0;
+        
+        
+        System.out.println(SALUTO_INIZIALE);
+        
+        Stella sole = Stella.creaSole();
+        
+        mostraMenu();
+    
+    
+    }
+    //**************************************************************************************
+
+
+
+    private static void mostraMenu() {
+        int scelta;
+        MyMenu menu = new MyMenu(TITOLO_MENU, VOCI_MENU);
+        
+        do {
+    
+            scelta = menu.scegli();
+            switch (scelta) {
+            case 1:
+                System.out.println(BENVENUTO_CREA_PIANETA);
+                creaCorpoCeleste();
+                
+                break;
+            case 2:
+                System.out.println(BENVENUTO_CREA_LUNA);
+                Pianeta p = creaCorpoCeleste();
+                break;
+            case 3:
+        //        eliminaPianeta();
+                break;
+            case 4:
+            //    eliminaLuna();
+                break;
+            default:
+                break;
+            }
+            
+        } while (scelta != 0);
+    }
+    
+    
+    
+    
+    
+    
+    public static Pianeta creaCorpoCeleste() {
+        
+        String codice = InputDati.leggiStringa(RICHIESTA_NOME_CORPO_CELESTE);
+        Vettore posizione = null;
+        posizione = leggiPosizione();
+        Double massa = InputDati.leggiDouble(RICHIESTA_MASSA_CORPO_CELESTE);
+        
+        
+        return new Pianeta(codice, posizione, massa);
+    }
+    
+
+    public static Vettore leggiPosizione() {
+        System.out.println(RICHIESTA_POSIZIONE_CORPO_CELESTE);
+        Double x = InputDati.leggiDouble("x = ");
+        Double y = InputDati.leggiDouble("y = ");
+        return new Vettore(x, y);
+    }
+
+}
+
+/*public class PlanetariumMain {
 
 	private static final String RICHIESTA_POSIZIONE_CORPO_CELESTE = "Inserisci la poszione del corpo celeste rispetto alla stella (0, 0): ";
 	private static final String RICHIESTA_MASSA_CORPO_CELESTE = "Inserisci la massa del corpo celeste: ";
@@ -109,4 +203,4 @@ public class PlanetariumMain {
 		return new Vettore(x, y);
 	}
 
-}
+}*/
